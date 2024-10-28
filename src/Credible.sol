856cd6f6db4ae9e32ae5ee95c56c27b73d4eca82
Odysseas.eth <odyslam@gmail.com>
2024-10-28 12:54:53 +0200

    chore: forge fmt

--- src/Credible.sol
+++ src/Credible.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {PhEvm} from "./PhEvm.sol";
 
-/// @notice The Credible contract               
+/// @notice The Credible contract
 contract Credible {
     //Precompile address -
     PhEvm ph = PhEvm(address(uint160(uint256(keccak256("Kim Jong Un Sucks")))));
