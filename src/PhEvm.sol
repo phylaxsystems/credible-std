34ab4e421f66995507a4a489bc534874151e61a7
formerly luehrsFred <luehrs.fred@gmail.com>
2025-01-08 17:37:17 +0100

    expose getLogs() (#4)
    * feat: add getLogs() to PhEVM
    
    * chore: add CODEOWNER file

--- src/PhEvm.sol
+++ src/PhEvm.sol
@@ -2,9 +2,22 @@
 pragma solidity ^0.8.13;
 
 interface PhEvm {
+    // An Ethereum log
+    struct Log {
+        // The topics of the log, including the signature, if any.
+        bytes32[] topics;
+        // The raw data of the log.
+        bytes data;
+        // The address of the log's emitter.
+        address emitter;
+    }
+
     //Forks to the state prior to the assertion triggering transaction.
     function forkPreState() external;
 
     //Forks to the state after the assertion triggering transaction.
     function forkPostState() external;
+
+    // Get the logs from the assertion triggering transaction.
+    function getLogs() external returns (Log[] memory logs);
 }
