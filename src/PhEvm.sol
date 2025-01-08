2ec07f0fbe21b0fd2bb75f57ebe0cddcf896dfe4
makemake <vukasin@gostovic.me>
2025-01-08 16:23:17 +0100

    feat: expose load precompile

--- src/PhEvm.sol
+++ src/PhEvm.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.13;
 
 interface PhEvm {
-    //Forks to the state prior to the assertion triggering transaction.
+    // Forks to the state prior to the assertion triggering transaction.
     function forkPreState() external;
 
-    //Forks to the state after the assertion triggering transaction.
+    // Forks to the state after the assertion triggering transaction.
     function forkPostState() external;
+
+    // Loads a storage slot from an address
+    function load(address target, bytes32 slot) external view returns (bytes32 data);
+
 }
