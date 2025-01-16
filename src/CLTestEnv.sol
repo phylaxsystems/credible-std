389f1f27c49709a23d6461185f654380d8989c63
formerly luehrsFred <luehrs.fred@gmail.com>
2025-01-16 13:48:11 +0100

    Create CL Test environment to allow execution in assertion executor (#6)
    * add AssertionTest
    
    * feat: create CredibleTest to allow execution of transactions in AssertionExecutor
    
    ---------
    
    Co-authored-by: Jacob Stenum Czepluch <j.czepluch@gmail.com>

---
+++ src/CLTestEnv.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {Vm} from "../lib/forge-std/src/Vm.sol";
+
+interface VmEx is Vm {
+    function assertionEx(bytes calldata tx, address assertionAdopter, bytes[] calldata assertions)
+        external
+        returns (bool success);
+}
+
+contract CLTestEnv {
+    struct AssertionTransaction {
+        address from;
+        address to;
+        uint256 value;
+        bytes data;
+    }
+
+    VmEx public immutable vmEx;
+    mapping(address => bytes[]) public adopters;
+
+    constructor(address vm_address) {
+        vmEx = VmEx(vm_address);
+    }
+
+    function addAssertion(address assertionAdopter, bytes memory assertionCode, bytes memory constructorArgs)
+        external
+    {
+        adopters[assertionAdopter].push(abi.encodePacked(assertionCode, constructorArgs));
+    }
+
+    function transact(address to, uint256 value, bytes calldata data) external returns (bool) {
+        return vmEx.assertionEx(
+            abi.encode(AssertionTransaction({from: msg.sender, to: to, value: value, data: data})), to, adopters[to]
+        );
+    }
+}
