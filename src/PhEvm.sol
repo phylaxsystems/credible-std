1b2abbcf93c959936f71e4dc2c1cc96ddb7472c2
fredo <luehrs.fred@gmail.com>
2025-01-20 14:09:28 +0100

    feat: Add getCallInputs to PhEVM precompile cheatcode

--- src/PhEvm.sol
+++ src/PhEvm.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 interface PhEvm {
-    // Forks to the state prior to the assertion triggering transaction.
     // An Ethereum log
     struct Log {
         // The topics of the log, including the signature, if any.
@@ -11,6 +10,32 @@ interface PhEvm {
         bytes data;
         // The address of the log's emitter.
         address emitter;
+    }
+
+    // Call inputs for the getCallInputs precompile
+    struct CallInputs {
+        // The call data of the call.
+        bytes input;
+        /// The gas limit of the call.
+        uint64 gas_limit;
+        // The account address of bytecode that is going to be executed.
+        //
+        // Previously `context.code_address`.
+        address bytecode_address;
+        // Target address, this account storage is going to be modified.
+        //
+        // Previously `context.address`.
+        address target_address;
+        // This caller is invoking the call.
+        //
+        // Previously `context.caller`.
+        address caller;
+        // Call value.
+        //
+        // NOTE: This value may not necessarily be transferred from caller to callee, see [`CallValue`].
+        //
+        // Previously `transfer.value` or `context.apparent_value`.
+        uint256 value;
     }
 
     //Forks to the state prior to the assertion triggering transaction.
@@ -24,4 +49,7 @@ interface PhEvm {
 
     // Get the logs from the assertion triggering transaction.
     function getLogs() external returns (Log[] memory logs);
+
+    // Get the call inputs for a given target and selector
+    function getCallInputs(address target, bytes4 selector) external view returns (CallInputs[] memory calls);
 }
