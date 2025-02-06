779f43bd12acfc38f01e272c36da99cf2ee1134e
Odysseas.eth <odyslam@gmail.com>
2025-02-06 17:19:08 +0200

    fix: pragma version

--- src/CredibleTest.sol
+++ src/CredibleTest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.13;
 
 import {Test} from "../lib/forge-std/src/Test.sol";
 import {console} from "../lib/forge-std/src/console.sol";
