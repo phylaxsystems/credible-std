bdaeda9a7b5a6673e6b444a1303905f0f53d3298
odysseas.eth <odyslam@gmail.com>
2025-02-06 16:45:35 +0100

    Feat/non return cheatcode (#10)
    * feat: update assertion ex interface
    
    * chore: improve var names
    
    * fix: pragma version

--- src/CLTestEnv.sol
+++ src/CLTestEnv.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity 0.8.13;
 
 import {Vm} from "../lib/forge-std/src/Vm.sol";
 
 interface VmEx is Vm {
-    function assertionEx(bytes calldata tx, address assertionAdopter, bytes[] calldata assertions)
-        external
-        returns (bool success, uint256 gasUsed, uint256 assertionsRan);
+    function assertionEx(bytes calldata tx, address assertionAdopter, bytes calldata assertions, string calldata label)
+        external;
 }
 
 contract CLTestEnv {
@@ -18,21 +17,33 @@ contract CLTestEnv {
     }
 
     VmEx public immutable vmEx;
-    mapping(address => bytes[]) public adopters;
+    mapping(string => bytes) public assertionLabelToAssertionContract;
+    mapping(string => address[]) public assertionLabelToAdopters;
 
     constructor(address vm_address) {
         vmEx = VmEx(vm_address);
     }
 
-    function addAssertion(address assertionAdopter, bytes memory assertionCode, bytes memory constructorArgs)
-        external
-    {
-        adopters[assertionAdopter].push(abi.encodePacked(assertionCode, constructorArgs));
+    function addAssertion(
+        string memory label,
+        address assertionAdopter,
+        bytes memory assertionCode,
+        bytes memory constructorArgs
+    ) external {
+        bytes memory assertion = abi.encodePacked(assertionCode, constructorArgs);
+        assertionLabelToAdopters[label].push(assertionAdopter);
+        assertionLabelToAssertionContract[label] = assertion;
     }
 
-    function validate(address to, uint256 value, bytes calldata data) external returns (bool, uint256, uint256) {
-        return vmEx.assertionEx(
-            abi.encode(AssertionTransaction({from: msg.sender, to: to, value: value, data: data})), to, adopters[to]
-        );
+    function validate(string memory label, address to, uint256 value, bytes calldata data) external {
+        for (uint256 i = 0; i < assertionLabelToAdopters[label].length; i++) {
+            address assertionAdopter = assertionLabelToAdopters[label][i];
+            vmEx.assertionEx(
+                abi.encode(AssertionTransaction({from: msg.sender, to: to, value: value, data: data})),
+                assertionAdopter,
+                assertionLabelToAssertionContract[label],
+                label
+            );
+        }
     }
 }
