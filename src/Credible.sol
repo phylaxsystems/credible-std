bc6b9a8878f234abda2acf12da4b51e7dde94857
GregTheDev <gbcurtis98@outlook.com>
2025-02-13 17:25:07 -0700

    feat: add trigger recorder

--- src/Credible.sol
+++ src/Credible.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {PhEvm} from "./PhEvm.sol";
 
 /// @notice The Credible contract
-contract Credible {
+abstract contract Credible {
     //Precompile address -
     PhEvm ph = PhEvm(address(uint160(uint256(keccak256("Kim Jong Un Sucks")))));
 }
