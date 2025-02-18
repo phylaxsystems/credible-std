92c7e0edc05cb6edba1c45d68489bc5d09c27b38
fredo <luehrs.fred@gmail.com>
2025-02-18 14:16:58 +0100

    feat: add additional trigger cheat codes

--- src/TriggerRecorder.sol
+++ src/TriggerRecorder.sol
@@ -2,6 +2,24 @@
 pragma solidity ^0.8.13;
 
 interface TriggerRecorder {
+    /// @notice Registers storage change trigger for all slots
+    /// @param fnSelector The function selector of the assertion function.
+    function registerStorageChangeTrigger(bytes4 fnSelector) external view;
+
+    /// @notice Registers storage change trigger for a slot
+    /// @param fnSelector The function selector of the assertion function.
+    /// @param slot The storage slot to trigger on.
+    function registerStorageChangeTrigger(bytes4 fnSelector, bytes32 slot) external view;
+
+    /// @notice Registers balance change trigger for the AA
+    /// @param fnSelector The function selector of the assertion function.
+    function registerBalanceChangeTrigger(bytes4 fnSelector) external view;
+
+    /// @notice Registers a call trigger for calls to the AA.
+    /// @param fnSelector The function selector of the assertion function.
+    /// @param triggerSelector The function selector of the trigger function.
+    function registerCallTrigger(bytes4 fnSelector, bytes4 triggerSelector) external view;
+
     /// @notice Records a call trigger for the specified assertion function.
     /// A call trigger signifies that the assertion function should be called
     /// if the assertion adopter is called.
