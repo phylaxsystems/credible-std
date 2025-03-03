ef082ab63492458a9b80bd3e71f6c598f2afaa4f
fredo <luehrs.fred@gmail.com>
2025-03-03 12:30:29 +0100

    fest: add helper functions for all trigger functions

--- src/Assertion.sol
+++ src/Assertion.sol
@@ -13,8 +13,36 @@ abstract contract Assertion is Credible, StateChanges {
     /// @notice Used to record fn selectors and their triggers.
     function triggers() external view virtual;
 
-    /// @notice Registers a call trigger for the specified assertion function.
+    /// @notice Registers a call trigger for the AA without specifying an AA function selector.
+    /// This will trigger the assertion function on any call to the AA.
+    /// @param fnSelector The function selector of the assertion function.
     function registerCallTrigger(bytes4 fnSelector) internal view {
         triggerRecorder.registerCallTrigger(fnSelector);
+    }
+
+    /// @notice Registers a call trigger for calls to the AA with a specific AA function selector.
+    /// @param fnSelector The function selector of the assertion function.
+    /// @param triggerSelector The function selector upon which the assertion will be triggered.
+    function registerCallTrigger(bytes4 fnSelector, bytes4 triggerSelector) internal view {
+        triggerRecorder.registerCallTrigger(fnSelector, triggerSelector);
+    }
+
+    /// @notice Registers storage change trigger for any slot
+    /// @param fnSelector The function selector of the assertion function.
+    function registerStorageChangeTrigger(bytes4 fnSelector) internal view {
+        triggerRecorder.registerStorageChangeTrigger(fnSelector);
+    }
+
+    /// @notice Registers storage change trigger for a specific slot
+    /// @param fnSelector The function selector of the assertion function.
+    /// @param slot The storage slot to trigger on.
+    function registerStorageChangeTrigger(bytes4 fnSelector, bytes32 slot) internal view {
+        triggerRecorder.registerStorageChangeTrigger(fnSelector, slot);
+    }
+
+    /// @notice Registers balance change trigger for the AA
+    /// @param fnSelector The function selector of the assertion function.
+    function registerBalanceChangeTrigger(bytes4 fnSelector) internal view {
+        triggerRecorder.registerBalanceChangeTrigger(fnSelector);
     }
 }
