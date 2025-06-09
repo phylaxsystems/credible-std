ced09d3ffb6761d11ee2eda4c11566c9f26c5719
GregTheDev <gbcurtis98@outlook.com>
2025-06-08 21:58:20 -0600

    fix: build issues

--- src/test-cases/precompiles/Logs.sol
+++ src/test-cases/precompiles/Logs.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import {Assertion} from "credible-std/Assertion.sol";
-import {PhEvm} from "credible-std/PhEvm.sol";
 import {Test} from "forge-std/Test.sol";
 import {console} from "forge-std/console.sol";
-import {Target, TARGET} from "./Target.sol";
+import {Assertion} from "../../Assertion.sol";
+import {PhEvm} from "../../PhEvm.sol";
+import {Target, TARGET} from "../common/Target.sol";
 
 contract TestGetLogs is Assertion, Test {
     constructor() payable {}
@@ -13,25 +13,21 @@ contract TestGetLogs is Assertion, Test {
     function testGetLogs() external {
         require(TARGET.readStorage() == 1, "val != 1");
         PhEvm.Log[] memory logs = ph.getLogs();
-        require(logs.length == 2, "logs.length != 2");
+        require(logs.length == 1, "logs.length != 2");
 
         PhEvm.Log memory log = logs[0];
         require(log.emitter == address(TARGET), "log.address != target");
         require(log.topics.length == 1, "log.topics.length != 1");
-        require(log.topics[0] == Target.Log.selector, "log.topics[0] != Target.Log.selector");
-        require(log.data.length == 32, "log.data.length != 32");
-        require(bytes32(log.data) == bytes32(uint256(1)), "log.data != 1");
-
-        log = logs[1];
-        require(log.emitter == address(TARGET), "log.address != target");
-        require(log.topics.length == 1, "log.topics.length != 1");
-        require(log.topics[0] == Target.Log2.selector, "log.topics[0] != Target.Log2.selector");
+        require(
+            log.topics[0] == Target.Log.selector,
+            "log.topics[0] != Target.Log.selector"
+        );
         require(log.data.length == 32, "log.data.length != 32");
         require(bytes32(log.data) == bytes32(uint256(1)), "log.data != 1");
     }
 
     function triggers() external view override {
-      registerCallTrigger(this.testGetLogs.selector);
+        registerCallTrigger(this.testGetLogs.selector);
     }
 }
 
