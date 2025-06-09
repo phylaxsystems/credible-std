f5f13ed384e37267b085eb73da5c18b2536539d4
GregTheDev <gbcurtis98@outlook.com>
2025-06-09 12:19:58 -0600

    feat: update Forking to new abstraction

--- src/test-cases/precompiles/Forking.sol
+++ src/test-cases/precompiles/Forking.sol
@@ -6,15 +6,45 @@ import {PhEvm} from "../../PhEvm.sol";
 import {Test} from "forge-std/Test.sol";
 
 import {Target, TARGET} from "../common/Target.sol";
+struct InitData {
+    address newTarget;
+    uint256 startValue;
+    uint256 expectedSumPersistence;
+}
 
-contract TestForking is Assertion, Test {
-    uint256 expectedSum = 0;
-    uint256 someInitValue = 1;
-    address newTarget = address(0x1A9c28714584DC5Bc4715C0624c171B5F4F82Be8);
+abstract contract ForkingTest is Assertion, Test {
+    uint256 public sum = 0;
+    uint256 public someInitValue = 1;
 
-    constructor() payable {}
+    InitData public initData;
+
+    constructor(InitData memory _initData) payable {
+        initData = _initData;
+    }
+
+    function testForkSwitchStorage() external {
+        uint256 startValue = initData.startValue;
+
+        //Test fork switching reads from underlying state
+        require(
+            TARGET.readStorage() == startValue + 1,
+            "readStorage() != startValue + 1"
+        );
+        ph.forkPreState();
+        uint256 preStateValue = TARGET.readStorage();
+        require(preStateValue == startValue, "preStateValue != startValue");
+
+        ph.forkPostState();
+        //Test fork switching reads from underlying state
+        require(
+            TARGET.readStorage() == startValue + 1,
+            "readStorage() != startValue + 1"
+        );
+    }
 
     function testForkSwitchNewDeployedContract() external {
+        address newTarget = initData.newTarget;
+
         require(
             newTarget.code.length != 0,
             "post state newTarget.code.length should not be 0"
@@ -33,77 +63,75 @@ contract TestForking is Assertion, Test {
         );
     }
 
-    //function testForkSwitch() external {
-    //    uint256 calls = ph
-    //        .getCallInputs(address(TARGET), TARGET.incrementStorage.selector)
-    //        .length;
+    function testPersistTargetContracts() external {
+        uint256 _startValue = initData.startValue;
+        uint256 _expectedSum = initData.expectedSumPersistence;
 
-    //    ph.forkPreState();
-    //    uint256 startValue = TARGET.readStorage();
-    //    require(startValue < 3, "startValue >= 3");
+        require(someInitValue == 1, "someInitValue != 1");
+        require(sum == 0, "expectedSum != 0");
 
-    //    ph.forkPostState();
-    //    //Test fork switching reads from underlying state
-    //    require(
-    //        TARGET.readStorage() == startValue + calls,
-    //        "readStorage() != startValue + calls"
-    //    );
+        require(
+            TARGET.readStorage() == _startValue + 1,
+            "val != _startValue + 1"
+        );
+        sum += TARGET.readStorage();
+        require(sum == _startValue + 1, "sum != _startValue + 1");
 
-    //    ph.forkPreState();
-    //    require(
-    //        TARGET.readStorage() == startValue,
-    //        "readStorage() != startValue"
-    //    );
+        ph.forkPreState();
+        require(
+            TARGET.readStorage() == _startValue,
+            "readStorage != _startValue"
+        );
+        sum += TARGET.readStorage();
+        require(sum == (_startValue * 2 + 1), "sum != _startValue * 2 + 1");
 
-    //    ph.forkPostState();
-    //    require(
-    //        TARGET.readStorage() == startValue + calls,
-    //        "readStorage() != startValue + calls"
-    //    );
-    //    require(
-    //        newTarget.code.length != 0,
-    //        "postState newTarget.code.length == 0"
-    //    );
+        ph.forkPostState();
+        require(
+            TARGET.readStorage() == _startValue + 1,
+            "val != _startValue + 1"
+        );
+        sum += TARGET.readStorage();
+        require(sum == ((_startValue * 3) + 2), "sum != _startValue * 2 + 2");
 
-    //    ph.forkPreState();
-    //    require(
-    //        TARGET.readStorage() == startValue,
-    //        "readStorage() != startValue"
-    //    );
-    //}
-
-    //function testPersistTargetContracts() external {
-    //    require(someInitValue == 1, "someInitValue != 1");
-    //    uint256 sum = 0;
-
-    //    require(TARGET.readStorage() == 2, "val != 2");
-    //    expectedSum += TARGET.readStorage();
-    //    sum += TARGET.readStorage();
-
-    //    ph.forkPreState();
-    //    require(TARGET.readStorage() == 1, "readStorage != 1");
-    //    expectedSum += TARGET.readStorage();
-    //    sum += TARGET.readStorage();
-
-    //    ph.forkPostState();
-    //    require(TARGET.readStorage() == 2, "val != 2");
-    //    expectedSum += TARGET.readStorage();
-    //    sum += TARGET.readStorage();
-
-    //    ph.forkPreState();
-    //    require(TARGET.readStorage() == 1, "val != 1");
-    //    expectedSum += TARGET.readStorage();
-    //    sum += TARGET.readStorage();
-
-    //    require(sum == expectedSum, "sum != expectedSum");
-    //    require(sum == 6, "sum != 6");
-    //}
+        ph.forkPreState();
+        require(
+            TARGET.readStorage() == _startValue,
+            "readStorage != _startValue"
+        );
+        sum += TARGET.readStorage();
+        require(sum == ((_startValue * 4) + 2), "sum != _startValue * 4 + 2");
+        require(sum == _expectedSum, "sum != _expectedSum");
+    }
 
     function triggers() external view override {
-        //registerCallTrigger(this.testForkSwitch.selector);
+        registerCallTrigger(this.testForkSwitchStorage.selector);
         registerCallTrigger(this.testForkSwitchNewDeployedContract.selector);
-        //registerCallTrigger(this.testPersistTargetContracts.selector);
+        registerCallTrigger(this.testPersistTargetContracts.selector);
     }
+}
+
+contract TestForking0 is ForkingTest {
+    constructor()
+        ForkingTest(
+            InitData({
+                newTarget: address(0x1A9c28714584DC5Bc4715C0624c171B5F4F82Be8),
+                startValue: 1,
+                expectedSumPersistence: 6
+            })
+        )
+    {}
+}
+
+contract TestForking2 is ForkingTest {
+    constructor()
+        ForkingTest(
+            InitData({
+                newTarget: address(0x1A9c28714584DC5Bc4715C0624c171B5F4F82Be8),
+                startValue: 2,
+                expectedSumPersistence: 10
+            })
+        )
+    {}
 }
 
 contract TriggeringTx {
