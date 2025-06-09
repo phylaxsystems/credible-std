b094d30d9cd906a16d02e1af19dd462daa52e17e
GregTheDev <gbcurtis98@outlook.com>
2025-06-08 23:41:00 -0600

    chore: debug Wip

--- src/test-cases/precompiles/Forking.sol
+++ src/test-cases/precompiles/Forking.sol
@@ -33,75 +33,76 @@ contract TestForking is Assertion, Test {
         );
     }
 
-    function testForkSwitch() external {
-        uint256 calls = ph
-            .getCallInputs(address(TARGET), TARGET.incrementStorage.selector)
-            .length;
+    //function testForkSwitch() external {
+    //    uint256 calls = ph
+    //        .getCallInputs(address(TARGET), TARGET.incrementStorage.selector)
+    //        .length;
 
-        ph.forkPreState();
-        uint256 startValue = TARGET.readStorage();
-        require(startValue < 3, "startValue >= 3");
+    //    ph.forkPreState();
+    //    uint256 startValue = TARGET.readStorage();
+    //    require(startValue < 3, "startValue >= 3");
 
-        ph.forkPostState();
-        //Test fork switching reads from underlying state
-        require(
-            TARGET.readStorage() == startValue + calls,
-            "readStorage() != startValue + calls"
-        );
+    //    ph.forkPostState();
+    //    //Test fork switching reads from underlying state
+    //    require(
+    //        TARGET.readStorage() == startValue + calls,
+    //        "readStorage() != startValue + calls"
+    //    );
 
-        ph.forkPreState();
-        require(
-            TARGET.readStorage() == startValue,
-            "readStorage() != startValue"
-        );
+    //    ph.forkPreState();
+    //    require(
+    //        TARGET.readStorage() == startValue,
+    //        "readStorage() != startValue"
+    //    );
 
-        ph.forkPostState();
-        require(
-            TARGET.readStorage() == startValue + calls,
-            "readStorage() != startValue + calls"
-        );
-        require(
-            newTarget.code.length != 0,
-            "postState newTarget.code.length == 0"
-        );
+    //    ph.forkPostState();
+    //    require(
+    //        TARGET.readStorage() == startValue + calls,
+    //        "readStorage() != startValue + calls"
+    //    );
+    //    require(
+    //        newTarget.code.length != 0,
+    //        "postState newTarget.code.length == 0"
+    //    );
 
-        ph.forkPreState();
-        require(
-            TARGET.readStorage() == startValue,
-            "readStorage() != startValue"
-        );
-    }
+    //    ph.forkPreState();
+    //    require(
+    //        TARGET.readStorage() == startValue,
+    //        "readStorage() != startValue"
+    //    );
+    //}
 
-    function testPersistTargetContracts() external {
-        require(someInitValue == 1, "someInitValue != 1");
-        uint256 sum = 0;
+    //function testPersistTargetContracts() external {
+    //    require(someInitValue == 1, "someInitValue != 1");
+    //    uint256 sum = 0;
 
-        require(TARGET.readStorage() == 2, "val != 2");
-        expectedSum += TARGET.readStorage();
-        sum += TARGET.readStorage();
+    //    require(TARGET.readStorage() == 2, "val != 2");
+    //    expectedSum += TARGET.readStorage();
+    //    sum += TARGET.readStorage();
 
-        ph.forkPreState();
-        require(TARGET.readStorage() == 1, "readStorage != 1");
-        expectedSum += TARGET.readStorage();
-        sum += TARGET.readStorage();
+    //    ph.forkPreState();
+    //    require(TARGET.readStorage() == 1, "readStorage != 1");
+    //    expectedSum += TARGET.readStorage();
+    //    sum += TARGET.readStorage();
 
-        ph.forkPostState();
-        require(TARGET.readStorage() == 2, "val != 2");
-        expectedSum += TARGET.readStorage();
-        sum += TARGET.readStorage();
+    //    ph.forkPostState();
+    //    require(TARGET.readStorage() == 2, "val != 2");
+    //    expectedSum += TARGET.readStorage();
+    //    sum += TARGET.readStorage();
 
-        ph.forkPreState();
-        require(TARGET.readStorage() == 1, "val != 1");
-        expectedSum += TARGET.readStorage();
-        sum += TARGET.readStorage();
+    //    ph.forkPreState();
+    //    require(TARGET.readStorage() == 1, "val != 1");
+    //    expectedSum += TARGET.readStorage();
+    //    sum += TARGET.readStorage();
 
-        require(sum == expectedSum, "sum != expectedSum");
-        require(sum == 6, "sum != 6");
-    }
+    //    require(sum == expectedSum, "sum != expectedSum");
+    //    require(sum == 6, "sum != 6");
+    //}
 
     function triggers() external view override {
-        registerCallTrigger(this.testForkSwitch.selector);
-        registerCallTrigger(this.testPersistTargetContracts.selector);
+        //registerCallTrigger(this.testForkSwitch.selector);
+        registerCallTrigger(this.testForkSwitchNewDeployedContract.selector);
+        //registerCallTrigger(this.testPersistTargetContracts.selector);
     }
 }
 
