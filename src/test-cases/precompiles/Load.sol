566d56f59f4abb7f7c01d064d559e0e00eaba29c
GregTheDev <gbcurtis98@outlook.com>
2025-06-24 11:15:52 -0500

    chore: fmt

--- src/test-cases/precompiles/Load.sol
+++ src/test-cases/precompiles/Load.sol
@@ -14,32 +14,19 @@ contract TestLoad is Assertion {
 
     function load() external {
         require(_loadCount() == 2, "postStateCount != 2 (no switch)");
-        require(
-            TARGET.readStorage() == _loadCount(),
-            "readStorage != postStateCount (no switch)"
-        );
+        require(TARGET.readStorage() == _loadCount(), "readStorage != postStateCount (no switch)");
 
         ph.forkPreState();
         require(_loadCount() == 1, "preStateCount != 1");
-        require(
-            TARGET.readStorage() == _loadCount(),
-            "readStorage != preStateCount"
-        );
+        require(TARGET.readStorage() == _loadCount(), "readStorage != preStateCount");
 
         ph.forkPostState();
         require(_loadCount() == 2, "postStateCount != 2 (switch)");
-        require(
-            TARGET.readStorage() == _loadCount(),
-            "readStorage != postStateCount (switch)"
-        );
+        require(TARGET.readStorage() == _loadCount(), "readStorage != postStateCount (switch)");
     }
 
     function loadRandomAccount() external view {
-        require(
-            ph.load(address(0x00000000000000000000000000000000001bd5a0), 0) ==
-                0,
-            "load(randomAccount) != 0"
-        );
+        require(ph.load(address(0x00000000000000000000000000000000001bd5a0), 0) == 0, "load(randomAccount) != 0");
     }
 
     function triggers() external view override {
